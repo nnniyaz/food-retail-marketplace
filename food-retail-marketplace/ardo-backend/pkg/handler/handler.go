03cacdf5fb22dfebf62da769cfe2d68b9c03372f
Niyaz Nassyrov <nassyrovich@gmail.com>
2023-05-02 19:37:57 +0600

    initial commit

---
+++ food-retail-marketplace/ardo-backend/pkg/handler/handler.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github/nnniyaz/ardo/pkg/service"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/login", h.login)
+		auth.POST("/logout", h.logout)
+	}
+
+	api := router.Group("/api")
+	{
+		users := api.Group("/users")
+		{
+			users.GET("/", h.getUsers)
+			users.GET("/:id", h.getUser)
+			users.POST("/", h.createUser)
+			users.PUT("/:id", h.updateUser)
+			users.DELETE("/:id", h.deleteUser)
+		}
+	}
+
+	return router
+}
