5c9e23da7c2338edf4c71dcdae6ecc4fa9123b7c
Niyaz Nassyrov <nassyrovich@gmail.com>
2024-04-19 12:14:04 +0500

    Content fix

--- food-retail-marketplace/ardo-landing/src/app/layout.tsx
+++ food-retail-marketplace/ardo-landing/src/app/layout.tsx
@@ -12,16 +12,16 @@ const roboto = Roboto({subsets: ["latin"], weight: ["100", "300", "400", "500", 
 
 export const metadata: Metadata = {
     metadataBase: new URL("https://app.ardogroup.org/"),
-    title: "Ardo Group Ltd.",
+    title: "Ardo",
     description: "ARDO is an AI-driven platform that helps F&B suppliers sell surplus at best prices. Online food vendor company for restaurants, cafes and hotels in Hong Kong - HK.",
     publisher: "Ardo Group Ltd.",
     creator: "Ardo Group Ltd.",
     manifest: "/site.webmanifest",
     openGraph: {
-        title: "Ardo Group Ltd. - From waste to value",
+        title: "Reduce food expenses of your restaurant, cafe or hotel with the help of Ardo Group Ltd. (Hong Kong - HK)",
         description: "ARDO is an AI-driven platform that helps F&B suppliers sell surplus at best prices. Online food vendor company for restaurants, cafes and hotels in Hong Kong - HK.",
         type: "website",
-        url: "https://app.ardogroup.org/",
+        url: "https://ardogroup.org/",
         images: [
             {
                 url: "https://ardodev.fra1.digitaloceanspaces.com/logos/ardo-logo.jpeg",
